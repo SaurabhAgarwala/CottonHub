f99216cfed226963c90fd67333b9bf9b0c14d8f1
SaurabhAgarwala <saur.agarwala@gmail.com>
2020-08-02 16:48:32 +0530

    Corrects an error

--- web_app/backend/api/api/views.py
+++ web_app/backend/api/api/views.py
@@ -105,61 +105,6 @@ def verifyOTP(request):
         return Response(data=data, status=status.HTTP_406_NOT_ACCEPTABLE)
 
 
-class CottonType(models.Model):
-    name = models.CharField(max_length=20, blank=False)
-    description = models.TextField(blank=True, null=False)
-
-    def __str__(self):
-        return self.name
-
-
-class Market(models.Model):
-    name = models.CharField(max_length=20, blank=False)
-    state = models.CharField(max_length=20, blank=False)
-    district = models.CharField(max_length=30, blank=False)
-
-    def __str__(self):
-        return self.name
-
-
-class Product(models.Model):
-    user = models.ForeignKey(User, default=1, blank=False, on_delete=models.CASCADE)
-    cotton_type = models.ForeignKey(CottonType, blank=False, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.user.first_name + "  " + self.cotton_type.name
-
-
-class Inventory(models.Model):
-    product = models.ForeignKey(Product, blank=False, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(blank=False)
-    selling_price = models.PositiveIntegerField(blank=False)
-    msp = models.PositiveIntegerField(blank=False)
-
-    def __str__(self):
-        return self.product.cotton_type.name + "  " + str(self.quantity)
-
-
-class Order(models.Model):
-    user = models.ForeignKey(User, default=1, blank=False, on_delete=models.CASCADE)
-    name = models.CharField(max_length=40, blank=False)
-    mobile = models.CharField(max_length=15, blank=False)
-    shipping_address = models.TextField(blank=False)
-    purchased = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.name + "  " + self.mobile
-
-
-class OrderItem(models.Model):
-    order = models.ForeignKey(Order, blank=False, on_delete=models.CASCADE)
-    inventory = models.ForeignKey(Inventory, blank=False, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(blank=False)
-
-    def __str__(self):
-        return self.order.name + "  " + self.inventory.product.cotton_type.name
-
-
 @permission_classes([IsAuthenticated])
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = models.Order.objects.all()
